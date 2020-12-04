38a894b8f8bab8e4b883fb694af9a8b787ecabdc
Jack Pennington <jcp3g17@soton.ac.uk>
2020-12-04 20:37:08 +0000

    Day3 part 2 and makefile

--- day3/day3.py
+++ day3/day3.py
@@ -25,7 +25,40 @@ def part_1():
 
 def part_2():
 
-    return -1
+    robo_turn = False
+    santa_loc = (0,0)
+    robo_loc = (0,0)
+
+    visited_houses = {(0,0)}
+
+    for d in directions:
+
+        if robo_turn:
+            loc = robo_loc
+        else:
+            loc = santa_loc
+
+        if d == '>':
+            loc = (loc[0]+1, loc[1])
+        elif d == '<':
+            loc = (loc[0]-1, loc[1])
+        elif d == '^':
+            loc = (loc[0], loc[1]+1)
+        else: # d == 'v'
+            loc = (loc[0], loc[1]-1)
+        
+        if loc not in visited_houses:
+            visited_houses.add(loc)
+        
+        if robo_turn:
+            robo_loc = loc
+        else:
+            santa_loc = loc
+        
+        robo_turn = not robo_turn
+    
+    return len(visited_houses)
 
 
-print("Part 1: " + part_1)
+print("Part 1: " + str(part_1()))
+print("Part 2: " + str(part_2()))
